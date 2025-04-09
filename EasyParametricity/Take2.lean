7e10f3a60e52b4e93c71a41bce62f5c538cd8d87
Jason Reed <jcreed@gmail.com>
2025-04-09 19:19:10 -0400

    Work backwards from the goal a bit

--- EasyParametricity/Take2.lean
+++ EasyParametricity/Take2.lean
@@ -6,6 +6,13 @@ import Mathlib.Logic.Function.Defs
 import Mathlib.Tactic.Find
 import Mathlib.Tactic.Have
 
+/-
+
+The proof idea here is not original to me; it's due to some
+yet-unpublished work by Jem Lord, see README.md for some links.
+
+-/
+
 open CategoryTheory
 
 universe v u
@@ -22,13 +29,6 @@ class Univalent (C : Type u) [Category C] where
 structure IsConst {A : Type u} {B : Type v} (h : A → B) where
   uval : B
   path : (a : A) → h a = uval
-
-/-
-
-The proof idea here is not original to me; it's due to some
-yet-unpublished work by Jem Lord, see README.md for some links.
-
--/
 
 variable 
    {C : Type v} [Category C] [Univalent C] [Limits.HasLimits.{u} C] 
@@ -63,15 +63,37 @@ Arguably the crucial idea of this proof is that there *is* an interesting functi
 U to the set of factorizations of a morphism in a U-complete category.
 -/
 class Unull (R : Type u) where
-  unull : Function.Bijective (λ (r : R) (_ : Type u) =>  r)
+  unull : (q : Type u → R) → IsConst q 
+
+/-
+This is an important construction. Given a factorization φ, and a type E, 
+we output a factorization such that...
+-/
+def mFunc (φ : Factor f) (E : Type u) : Factor f := sorry
+
+/-
+... M(0) = (f, id) and...
+-/
+theorem factor_lemma_zero (φ : Factor f) : mFunc f φ PEmpty = idFac f := sorry
+/-
+... M(1) = φ 
+-/
+theorem factor_lemma_one (φ : Factor f) : mFunc f φ PUnit = φ := sorry
 
 /-
 If f is a morphism in a U-univalent U-complete category, then any function z : fact(f) → R
 is constant.
 -/
 def factorToUnullValue (R : Type u) [Unull R] (z : Factor f → R) (φ : Factor f) :
-    z φ = z (idFac f) :=
- sorry
+        z φ = z (idFac f) := 
+    let M : (E : Type u) → Factor f := mFunc f φ
+    let compositeIsConst := Unull.unull (z ∘ M)
+    let eqn : z (M PUnit) = z (M PEmpty) := 
+       Trans.trans (compositeIsConst.path PUnit)
+                   (symm (compositeIsConst.path PEmpty))
+    let eq1 : M PUnit = φ := factor_lemma_one f φ
+    let eq0 : M PEmpty = idFac f := factor_lemma_zero f φ
+    by rw [eq0, eq1] at eqn; exact eqn
 
 /-
  This is the main interesting lemma from which some particular parametricity results will follow.
