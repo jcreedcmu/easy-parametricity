3dd3e942b016990bbf64b7d556e73b559f45c979
Jason Reed <jcreed@gmail.com>
2025-04-09 22:13:44 -0400

    Delete this again

--- EasyParametricity/Lemmas.lean
+++
@@ -1,6 +0,0 @@
-omit [Limits.HasLimits C] in 
-theorem one_limit_iso {J : Type} [Category J]  (F : J ⥤ C) [Limits.HasLimit F]
-         (A : Limits.LimitCone F) : Limits.limit F = A.cone.pt := 
-  by
-   apply Univalent.univalence
-   exact (Limits.limit.isoLimitCone A)
